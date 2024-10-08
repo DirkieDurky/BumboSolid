d1c6fcdcf9d3071050c7e085957d9784b693e19c
DirkieDurky <dirk@freijters.nl>
2024-10-08 11:16:50 +0200

    Initial commit

---
+++ bumbo-solid/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bumbo_solid.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
