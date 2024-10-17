04ffd2929d5f25a5af5f52e9efda85bcb317aa8b
CollinSteeg <167075925+CollinSteeg@users.noreply.github.com>
2024-10-17 17:48:27 +0200

    fixed saving norms to database

--- BumboSolid.Web/Controllers/NormeringenController.cs
+++ BumboSolid.Web/Controllers/NormeringenController.cs
@@ -37,28 +37,44 @@ namespace BumboSolid.Web.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Aanmaken(Norm norm, string DurationUnit)
         {
+            var selectedFunction = _context.Functions.FirstOrDefault(f => f.Name == norm.Function);
+
+            if (selectedFunction == null)
+            {
+                ModelState.AddModelError("Function", "The selected function is not valid.");
+                ViewBag.Function = new SelectList(_context.Functions.Select(f => f.Name).ToList());
+                ViewBag.TimeUnits = new SelectList(new List<string> { "Seconden", "Minuten", "Uren" });
+                return View(norm);
+            }
+
+            norm.FunctionNavigation = selectedFunction;
+
+            // Convert Duration to seconds based on the selected DurationUnit
+            switch (DurationUnit.ToLower())
+            {
+                case "minutes":
+                    norm.Duration *= 60;
+                    break;
+                case "hours":
+                    norm.Duration *= 3600;
+                    break;
+            }
+
+            // Check if the model state is still valid before saving to the database
             if (ModelState.IsValid)
             {
-                // Convert Duration to seconds based on the selected DurationUnit
-                switch (DurationUnit)
-                {
-                    case "minutes":
-                        norm.Duration *= 60;
-                        break;
-                    case "hours":
-                        norm.Duration *= 3600;
-                        break;
-                }
-
                 _context.Norms.Add(norm);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
 
-            ViewBag.Function = new SelectList(new List<string> { "Vers", "Kassa", "Vakkenvullen" });
+            ViewBag.Function = new SelectList(_context.Functions.Select(f => f.Name).ToList());
             ViewBag.TimeUnits = new SelectList(new List<string> { "Seconden", "Minuten", "Uren" });
             return View(norm);
         }
+
+
+
 
 
         // POST: Normeringen/Bewerken/5
