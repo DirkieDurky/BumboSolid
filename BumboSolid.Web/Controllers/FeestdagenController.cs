067c64cd9e76a32fea06cccd879bc26b27c6c89c
“Your <you@example.com>
2024-10-17 14:39:37 +0200

    The holiday overview now shows all the created holidays

--- BumboSolid.Web/Controllers/FeestdagenController.cs
+++ BumboSolid.Web/Controllers/FeestdagenController.cs
@@ -1,14 +1,40 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BumboSolid.Data;
+using BumboSolid.Data.Models;
+using BumboSolid.Web.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BumboSolid.Web.Controllers
 {
 	public class FeestdagenController : Controller
 	{
+		private readonly BumboDbContext _context;
+
+		public FeestdagenController(BumboDbContext context)
+		{
+			_context = context;
+		}
+
 		// GET: FeestdagenController
 		public ActionResult Index()
 		{
-			return View();
+			List<HolidayViewModel> holidayViewModels = new List<HolidayViewModel>();
+
+			foreach (var holiday in _context.Holidays.Include(x => x.HolidayDays))
+			{
+				List<HolidayDay> holidayDays = holiday.HolidayDays.ToList();
+
+				DateOnly firstDay = holidayDays.Min(x => x.Date);
+
+				DateOnly lastDay = holidayDays.Max(x => x.Date);
+
+				HolidayViewModel holidayViewModel = new HolidayViewModel() { Holiday = holiday, FirstDay = firstDay, LastDay = lastDay };
+
+				holidayViewModels.Add(holidayViewModel);
+			}
+
+			return View(holidayViewModels);
 		}
 
 		// GET: FeestdagenController/Details/5
