40004e6690cdde4edc1ddedec62f60e69b874337
CollinSteeg <167075925+CollinSteeg@users.noreply.github.com>
2024-10-18 20:19:35 +0200

    added edit functionality

--- BumboSolid.Web/Controllers/NormeringenController.cs
+++ BumboSolid.Web/Controllers/NormeringenController.cs
@@ -77,20 +77,74 @@ namespace BumboSolid.Web.Controllers
             return View(norm);
         }
 
-        // POST: Normeringen/Bewerken/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Bewerken(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		// GET: Normeringen/Bewerken/5
+		public async Task<IActionResult> Bewerken(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var norm = await _context.Norms.FindAsync(id);
+			if (norm == null)
+			{
+				return NotFound();
+			}
+
+			ViewBag.Function = new SelectList(new List<string> { "Vers", "Kassa", "Vakkenvullen" }, norm.Function);
+			ViewBag.TimeUnits = new SelectList(new List<string> { "Seconden", "Minuten", "Uren" });
+
+			return View(norm);
+		}
+
+		// POST: Normeringen/Bewerken/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Bewerken(int id, Norm norm, string DurationUnit)
+		{
+			if (id != norm.Id)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					// Convert Duration to seconds based on the selected DurationUnit
+					switch (DurationUnit.ToLower())
+					{
+						case "minutes":
+							norm.Duration *= 60;
+							break;
+						case "hours":
+							norm.Duration *= 3600;
+							break;
+					}
+
+					_context.Update(norm);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!_context.Norms.Any(e => e.Id == norm.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+
+			ViewBag.Function = new SelectList(new List<string> { "Vers", "Kassa", "Vakkenvullen" }, norm.Function);
+			ViewBag.TimeUnits = new SelectList(new List<string> { "Seconden", "Minuten", "Uren" });
+			return View(norm);
+		}
+
+
 
 		// GET: Normeringen/Verwijderen/5
 		public async Task<IActionResult> Verwijderen(int? id)
