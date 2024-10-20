3aa4c612cbf2bf78cfb111f6db6ba8db4300820c
CollinSteeg <167075925+CollinSteeg@users.noreply.github.com>
2024-10-20 13:24:02 +0200

    added  norm restrictions

--- BumboSolid.Data/Models/Norm.cs
+++ BumboSolid.Data/Models/Norm.cs
@@ -9,12 +9,15 @@ public partial class Norm
     [Key]
     public int Id { get; set; }
 
+    [StringLength(50)]
     public string Activity { get; set; } = null!;
 
     public string Function { get; set; } = null!;
 
+    [Range(0, int.MaxValue, ErrorMessage = "Duration must be a non-negative value.")]
     public int Duration { get; set; }
 
+    [Range(0, byte.MaxValue, ErrorMessage = "Average Daily Performances must be a non-negative value.")]
     public byte AvgDailyPerformances { get; set; }
 
     public bool PerVisitor { get; set; }
