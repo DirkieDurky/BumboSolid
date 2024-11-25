c2c0cab8eef27c90cf11b59f1cb1b6c1716bf4fe
“Your <you@example.com>
2024-11-25 21:07:22 +0100

    added views

---
+++ Controllers/ScheduleEmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using BumboSolid.Data;
+using BumboSolid.Data.Models;
+using BumboSolid.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BumboSolid.Controllers
+{
+    [Authorize(Roles = "Manager")]
+    [Route("Schedule")]
+	public class ScheduleEmployeeController : Controller
+	{
+		private readonly BumboDbContext _context;
+
+		public ScheduleEmployeeController(BumboDbContext context)
+		{
+			_context = context;
+		}
+
+        // GET: ScheduleEmployeeController
+        [HttpGet("")]
+		public ActionResult Schedule()
+		{
+            return View();
+		}
+
+        // GET: ScheduleEmployeeController/OutgoingFillRequests
+        [HttpGet("Uitgaande invalsverzoeken")]
+        public ActionResult OutgoingFillRequests()
+        {
+            var shifts = _context.Shifts.ToList(); //.Where(e => e.Employee == User.Id)
+
+            List<FillRequestViewModel> fillRequests = new List<FillRequestViewModel>();
+
+			foreach (Shift shift in shifts)
+            {
+                foreach (FillRequest fillRequest in shift.FillRequests)
+                {
+					FillRequestViewModel fillRequestViewModel = new FillRequestViewModel()
+					{
+
+
+				     };
+				}
+			}
+			}
+
+            return View(shifts);
+        }
+
+        // GET: ScheduleEmployeeController/IncomingFillRequests
+        [HttpGet("Inkomende invalsverzoeken")]
+        public ActionResult IncomingFillRequests()
+        {
+            return View();
+        }
+    }
+}
--- Models/FillRequestViewModel.cs
+++ Models/FillRequestViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace BumboSolid.Models
+﻿using BumboSolid.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BumboSolid.Models
 {
     public class FillRequestViewModel
     {
-    }
+		public DateOnly Date { get; set; }
+
+		public String Day { get; set; }
+
+		public string Department { get; set; }
+
+		public TimeOnly StartTime { get; set; }
+
+		public TimeOnly EndTime { get; set; }
+
+		public string Accepted { get; set; }
+	}
 }
