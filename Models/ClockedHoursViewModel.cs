3199cdc2a78d8b7e55c8869ed8a2ba248492816c
CollinSteeg <167075925+CollinSteeg@users.noreply.github.com>
2024-12-20 23:56:56 +0100

    Added clocking screen employees

---
+++ Models/ClockedHoursViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BumboSolid.Data.Models;
+
+namespace BumboSolid.Models
+{
+    public class ClockedHoursViewModel
+    {
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public virtual List<ClockedHours> ClockedHours { get; set; } = new List<ClockedHours>();
+
+        public Dictionary<byte, string> WeekdayDictionary { get; set; }
+
+        public List<Department> Departments { get; set; } = new List<Department>();
+
+        public List<string> SelectedDepartments { get; set; } = new List<string>();
+    }
+}
