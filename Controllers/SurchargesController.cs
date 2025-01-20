b4ab20a6e7f401fed0bedbc5300fe50017c7a728
Niels Bouwman <nsw.bouwman@student.avans.nl>
2025-01-20 15:40:24 +0100

    Added Surcharge page
    Surcharge page is added however it does not yet contain data.

---
+++ Controllers/SurchargesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BumboSolid.Controllers
+{
+	public class SurchargesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
