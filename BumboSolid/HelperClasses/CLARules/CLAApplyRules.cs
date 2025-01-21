b8e5026a9139852598c58e8d2d3f662bdbf33676
Janno <165158587+BaksteenV20@users.noreply.github.com>
2025-01-21 15:37:10 +0100

    added unit tests

--- BumboSolid/HelperClasses/CLARules/CLAApplyRules.cs
+++ BumboSolid/HelperClasses/CLARules/CLAApplyRules.cs
@@ -10,52 +10,77 @@ namespace BumboSolid.HelperClasses.CLARules
 
 			foreach (CLAEntry CLA in CLAs)
 			{
-				// Shift duration
-				if (ShiftDuration(shift, CLA) == false) validShift = false;
-
-				// Average works hours over a span of 4 weeks
-				if (AvgWorkHoursOverFourWeeks(shift, CLA, shifts, userId) == false) validShift = false;
-
-				// Latest work time
-				if (shift.EndTime > CLA.LatestWorkTime) validShift = false;
-
-				// Earliest work time
-				if (shift.StartTime < CLA.EarliestWorkTime) validShift = false;
-
-				// Max work duration per week
 				var thisWeekShifts = shifts.Where(s => s.EmployeeId == userId && shift.Week.WeekNumber == s.Week.WeekNumber && s.Week.Year == shift.Week.Year).ToList();
-				var thisWeekTotalMinutes = (shift.EndTime - shift.StartTime).Minutes;
-				foreach (Shift pastShift in thisWeekShifts) thisWeekTotalMinutes = thisWeekTotalMinutes + (pastShift.EndTime - pastShift.StartTime).Minutes;
-				if (thisWeekTotalMinutes > CLA.MaxWorkDurationPerWeek) validShift = false;
 
-				// Max work days per week
-				List<int> workDays = new List<int>();
-				workDays.Add(shift.Weekday);
-				foreach (Shift pastShift in thisWeekShifts) if (workDays.Contains(pastShift.Weekday) == false) workDays.Add(pastShift.Weekday);
-				if (workDays.Count > CLA.MaxWorkDaysPerWeek) validShift = false;
-
-				// Max work duration per day
-				var todayShifts = shifts.Where(s => s.EmployeeId == userId && shift.Weekday == s.Weekday && shift.Week.WeekNumber == s.Week.WeekNumber && s.Week.Year == shift.Week.Year).ToList();
-				var todayTotalMinutes = (shift.EndTime - shift.StartTime).Minutes;
-				foreach (Shift pastShift in todayShifts) todayTotalMinutes = todayTotalMinutes + (pastShift.EndTime - pastShift.StartTime).Minutes;
-				if (todayTotalMinutes > CLA.MaxWorkDurationPerDay) validShift = false;
+				if (ShiftDuration(shift, CLA) == false) validShift = false;
+				if (AvgWorkHoursOverFourWeeks(shift, CLA, shifts, userId) == false) validShift = false;
+				if (LatestWorkTime(shift, CLA) == false) validShift = false;
+				if (EarliestWorkTime(shift, CLA) == false) validShift = false;
+				if (MaxWorkDurationPerWeek(shift, CLA, thisWeekShifts) == false) validShift = false;
+				if (MaxWorkDaysPerWeek(shift, CLA, thisWeekShifts) == false) validShift = false;
+				if (MaxWorkDurationPerDay(shift, CLA, shifts, userId) == false) validShift = false;
 			}
 
 			return validShift;
 		}
 
+		// Shift duration
 		public bool ShiftDuration(Shift shift, CLAEntry CLA)
 		{
 			if ((shift.EndTime - shift.StartTime).TotalMinutes > CLA.MaxShiftDuration) return false;
 			return true;
 		}
 
+		// Average works hours over a span of 4 weeks
 		public bool AvgWorkHoursOverFourWeeks(Shift shift, CLAEntry CLA, List<Shift> shifts, int userId)
 		{
 			var lastFourWeeksShifts = shifts.Where(s => s.EmployeeId == userId && shift.Week.WeekNumber - s.Week.WeekNumber < 4 && s.Week.Year == shift.Week.Year).ToList();
 			var lastFourWeeksTotalMinutes = (shift.EndTime - shift.StartTime).TotalMinutes;
 			foreach (Shift pastShift in lastFourWeeksShifts) lastFourWeeksTotalMinutes = lastFourWeeksTotalMinutes + (pastShift.EndTime - pastShift.StartTime).TotalMinutes;
 			if (lastFourWeeksTotalMinutes > (CLA.MaxAvgWeeklyWorkDurationOverFourWeeks*4)) return false;
+			return true;
+		}
+
+		// Latest work time
+		public bool LatestWorkTime(Shift shift, CLAEntry CLA)
+		{
+			if (shift.EndTime > CLA.LatestWorkTime) return false;
+			return true;
+		}
+
+		// Earliest work time
+		public bool EarliestWorkTime(Shift shift, CLAEntry CLA)
+		{
+			if (shift.StartTime < CLA.EarliestWorkTime) return false;
+			return true;
+		}
+
+		// Max work duration per week
+		public bool MaxWorkDurationPerWeek(Shift shift, CLAEntry CLA, List<Shift> shifts)
+		{
+			var thisWeekTotalMinutes = (shift.EndTime - shift.StartTime).TotalMinutes;
+			foreach (Shift pastShift in shifts) thisWeekTotalMinutes = thisWeekTotalMinutes + (pastShift.EndTime - pastShift.StartTime).TotalMinutes;
+			if (thisWeekTotalMinutes > CLA.MaxWorkDurationPerWeek) return false;
+			return true;
+		}
+
+		// Max work days per week
+		public bool MaxWorkDaysPerWeek(Shift shift, CLAEntry CLA, List<Shift> shifts)
+		{
+			List<int> workDays = new List<int>();
+			workDays.Add(shift.Weekday);
+			foreach (Shift pastShift in shifts) if (workDays.Contains(pastShift.Weekday) == false) workDays.Add(pastShift.Weekday);
+			if (workDays.Count > CLA.MaxWorkDaysPerWeek) return false;
+			return true;
+		}
+
+		// Max work duration per day
+		public bool MaxWorkDurationPerDay(Shift shift, CLAEntry CLA, List<Shift> shifts, int userId)
+		{
+			var todayShifts = shifts.Where(s => s.EmployeeId == userId && shift.Weekday == s.Weekday && shift.Week.WeekNumber == s.Week.WeekNumber && s.Week.Year == shift.Week.Year).ToList();
+			var todayTotalMinutes = (shift.EndTime - shift.StartTime).TotalMinutes;
+			foreach (Shift pastShift in todayShifts) todayTotalMinutes = todayTotalMinutes + (pastShift.EndTime - pastShift.StartTime).TotalMinutes;
+			if (todayTotalMinutes > CLA.MaxWorkDurationPerDay) return false;
 			return true;
 		}
 	}
--- UnitTests/CLARulesTests.cs
+++ UnitTests/CLARulesTests.cs
@@ -63,20 +63,20 @@ namespace UnitTests
 			Shift firstShift = new()
 			{
 				StartTime = new TimeOnly(12, 00),
-				EndTime = new TimeOnly(21, 00),
+				EndTime = new TimeOnly(13, 00),
 				EmployeeId = 1,
 				Week = firstWeek
 			};
 			Shift secondShift = new()
 			{
 				StartTime = new TimeOnly(12, 00),
-				EndTime = new TimeOnly(18, 00),
+				EndTime = new TimeOnly(15, 00),
 				EmployeeId = 1,
 				Week = secondWeek
 			};
 			CLAEntry rule = new()
 			{
-				MaxAvgWeeklyWorkDurationOverFourWeeks = 1
+				MaxAvgWeeklyWorkDurationOverFourWeeks = 60
 			};
 			CLAApplyRules validator = new();
 
@@ -84,6 +84,343 @@ namespace UnitTests
 			bool result = validator.AvgWorkHoursOverFourWeeks(secondShift, rule, model, 1);
 
 			Assert.True(result);
+		}
+
+		// Adding shift in a four week period adding up to be higher than the maximum average should return false
+		[Fact]
+		public void Create_AvgShiftDurationOverFourWeekPeriodAboveMaximum_False()
+		{
+			Week firstWeek = new()
+			{
+				WeekNumber = 1,
+				Year = 2025
+			};
+			Week secondWeek = new()
+			{
+				WeekNumber = 2,
+				Year = 2025
+			};
+			List<Shift> model = new();
+			Shift firstShift = new()
+			{
+				StartTime = new TimeOnly(12, 00),
+				EndTime = new TimeOnly(21, 00),
+				EmployeeId = 1,
+				Week = firstWeek
+			};
+			Shift secondShift = new()
+			{
+				StartTime = new TimeOnly(12, 00),
+				EndTime = new TimeOnly(20, 00),
+				EmployeeId = 1,
+				Week = secondWeek
+			};
+			CLAEntry rule = new()
+			{
+				MaxAvgWeeklyWorkDurationOverFourWeeks = 120
+			};
+			CLAApplyRules validator = new();
+
+			model.Add(firstShift);
+			bool result = validator.AvgWorkHoursOverFourWeeks(secondShift, rule, model, 1);
+
+			Assert.False(result);
+		}
+
+		// Adding shift with an end time lower than the latest allowed work time should return true
+		[Fact]
+		public void Create_ShiftEndTimeBelowStopTime_True()
+		{
+			Shift shiftModel = new()
+			{
+				StartTime = new TimeOnly(13, 00),
+				EndTime = new TimeOnly(17, 00),
+			};
+			CLAEntry rule = new()
+			{
+				LatestWorkTime = new TimeOnly(18, 00),
+			};
+			CLAApplyRules validator = new();
+
+			bool result = validator.LatestWorkTime(shiftModel, rule);
+
+			Assert.True(result);
+		}
+
+		// Adding shift with an end time higher than the latest allowed work time should return false
+		[Fact]
+		public void Create_ShiftEndTimeAboveStopTime_False()
+		{
+			Shift shiftModel = new()
+			{
+				StartTime = new TimeOnly(12, 00),
+				EndTime = new TimeOnly(19, 00),
+			};
+			CLAEntry rule = new()
+			{
+				LatestWorkTime = new TimeOnly(13, 00),
+			};
+			CLAApplyRules validator = new();
+
+			bool result = validator.LatestWorkTime(shiftModel, rule);
+
+			Assert.False(result);
+		}
+
+		// Adding shift with a start time higher than the earliest allowed work time should return true
+		[Fact]
+		public void Create_ShiftStartTimeAboveStartTime_True()
+		{
+			Shift shiftModel = new()
+			{
+				StartTime = new TimeOnly(9, 00),
+				EndTime = new TimeOnly(11, 00),
+			};
+			CLAEntry rule = new()
+			{
+				EarliestWorkTime = new TimeOnly(7, 00),
+			};
+			CLAApplyRules validator = new();
+
+			bool result = validator.EarliestWorkTime(shiftModel, rule);
+
+			Assert.True(result);
+		}
+
+		// Adding shift with a start time lower than the earliest allowed work time should return false
+		[Fact]
+		public void Create_ShiftStartTimeBelowStartTime_False()
+		{
+			Shift shiftModel = new()
+			{
+				StartTime = new TimeOnly(7, 00),
+				EndTime = new TimeOnly(13, 00),
+			};
+			CLAEntry rule = new()
+			{
+				EarliestWorkTime = new TimeOnly(9, 00),
+			};
+			CLAApplyRules validator = new();
+
+			bool result = validator.EarliestWorkTime(shiftModel, rule);
+
+			Assert.False(result);
+		}
+
+		// Adding shift in a week period adding up to be lower than the maximum allowed time should return true
+		[Fact]
+		public void Create_TotalShiftDurationBelowMaximumInWeek_True()
+		{
+			Week week = new()
+			{
+				WeekNumber = 1,
+				Year = 2025
+			};
+			List<Shift> model = new();
+			Shift firstShift = new()
+			{
+				StartTime = new TimeOnly(9, 00),
+				EndTime = new TimeOnly(11, 00),
+				Week = week
+			};
+			Shift secondShift = new()
+			{
+				StartTime = new TimeOnly(14, 00),
+				EndTime = new TimeOnly(17, 00),
+				Week = week
+			};
+			CLAEntry rule = new()
+			{
+				MaxWorkDurationPerWeek = 600
+			};
+			CLAApplyRules validator = new();
+
+			model.Add(firstShift);
+			bool result = validator.MaxWorkDurationPerWeek(secondShift, rule, model);
+
+			Assert.True(result);
+		}
+
+		// Adding shift in a week period adding up to be higher than the maximum allowed time should return false
+		[Fact]
+		public void Create_TotalShiftDurationAboveMaximumInWeek_False()
+		{
+			Week week = new()
+			{
+				WeekNumber = 1,
+				Year = 2025
+			};
+			List<Shift> model = new();
+			Shift firstShift = new()
+			{
+				StartTime = new TimeOnly(7, 00),
+				EndTime = new TimeOnly(13, 00),
+				Week = week
+			};
+			Shift secondShift = new()
+			{
+				StartTime = new TimeOnly(9, 00),
+				EndTime = new TimeOnly(15, 00),
+				Week = week
+			};
+			CLAEntry rule = new()
+			{
+				MaxWorkDurationPerWeek = 120
+			};
+			CLAApplyRules validator = new();
+
+			model.Add(firstShift);
+			bool result = validator.MaxWorkDurationPerWeek(secondShift, rule, model);
+
+			Assert.False(result);
+		}
+
+		// Adding shift in a week period adding up to be lower than the maximum allowed shifts should return true
+		[Fact]
+		public void Create_TotalShiftsBelowMaximum_True()
+		{
+			Week week = new()
+			{
+				WeekNumber = 1,
+				Year = 2025
+			};
+			List<Shift> model = new();
+			Shift firstShift = new()
+			{
+				StartTime = new TimeOnly(11, 00),
+				EndTime = new TimeOnly(12, 00),
+				Week = week,
+				Weekday = 5
+			};
+			Shift secondShift = new()
+			{
+				StartTime = new TimeOnly(9, 00),
+				EndTime = new TimeOnly(18, 00),
+				Week = week,
+				Weekday = 5
+			};
+			CLAEntry rule = new()
+			{
+				MaxWorkDaysPerWeek = 1
+			};
+			CLAApplyRules validator = new();
+
+			model.Add(firstShift);
+			bool result = validator.MaxWorkDaysPerWeek(secondShift, rule, model);
+
+			Assert.True(result);
+		}
+
+		// Adding shift in a week period adding up to be higher than the maximum allowed shifts should return false
+		[Fact]
+		public void Create_TotalShiftsAboveMaximum_False()
+		{
+			Week week = new()
+			{
+				WeekNumber = 1,
+				Year = 2025
+			};
+			List<Shift> model = new();
+			Shift firstShift = new()
+			{
+				StartTime = new TimeOnly(7, 00),
+				EndTime = new TimeOnly(13, 00),
+				Week = week,
+				Weekday = 1
+			};
+			Shift secondShift = new()
+			{
+				StartTime = new TimeOnly(16, 00),
+				EndTime = new TimeOnly(17, 00),
+				Week = week,
+				Weekday = 2
+			};
+			CLAEntry rule = new()
+			{
+				MaxWorkDaysPerWeek = 1
+			};
+			CLAApplyRules validator = new();
+
+			model.Add(firstShift);
+			bool result = validator.MaxWorkDaysPerWeek(secondShift, rule, model);
+
+			Assert.False(result);
+		}
+
+		// Adding shift in a day period adding up to be lower than the maximum allowed time should return true
+		[Fact]
+		public void Create_TotalShiftDurationBelowMaximumInDay_True()
+		{
+			Week week = new()
+			{
+				WeekNumber = 1,
+				Year = 2025
+			};
+			List<Shift> model = new();
+			Shift firstShift = new()
+			{
+				StartTime = new TimeOnly(9, 00),
+				EndTime = new TimeOnly(10, 00),
+				Week = week,
+				Weekday = 2,
+				EmployeeId = 1
+			};
+			Shift secondShift = new()
+			{
+				StartTime = new TimeOnly(10, 00),
+				EndTime = new TimeOnly(11, 00),
+				Week = week,
+				Weekday = 2,
+				EmployeeId = 1
+			};
+			CLAEntry rule = new()
+			{
+				MaxWorkDurationPerDay = 120
+			};
+			CLAApplyRules validator = new();
+
+			model.Add(firstShift);
+			bool result = validator.MaxWorkDurationPerDay(secondShift, rule, model, 1);
+
+			Assert.True(result);
+		}
+
+		// Adding shift in a day period adding up to be higher than the maximum allowed time should return false
+		[Fact]
+		public void Create_TotalShiftDurationAboveMaximumInDay_False()
+		{
+			Week week = new()
+			{
+				WeekNumber = 1,
+				Year = 2025
+			};
+			List<Shift> model = new();
+			Shift firstShift = new()
+			{
+				StartTime = new TimeOnly(7, 00),
+				EndTime = new TimeOnly(9, 00),
+				Week = week,
+				Weekday = 2,
+				EmployeeId = 1
+			};
+			Shift secondShift = new()
+			{
+				StartTime = new TimeOnly(12, 00),
+				EndTime = new TimeOnly(13, 00),
+				Week = week,
+				Weekday = 2,
+				EmployeeId = 1
+			};
+			CLAEntry rule = new()
+			{
+				MaxWorkDurationPerDay = 120
+			};
+			CLAApplyRules validator = new();
+
+			model.Add(firstShift);
+			bool result = validator.MaxWorkDurationPerDay(secondShift, rule, model, 1);
+
+			Assert.False(result);
 		}
 	}
 }
