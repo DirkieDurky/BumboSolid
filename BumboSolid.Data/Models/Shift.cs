170390fa0a88ac8dbbfd1e7c193d3bd767fa74a6
DirkieDurky <dirk@freijters.nl>
2025-01-24 02:03:34 +0100

    Separate data to it's own class library

--- BumboSolid.Data/Models/Shift.cs
+++ BumboSolid.Data/Models/Shift.cs
@@ -26,7 +26,7 @@ public class Shift
 
     public virtual List<FillRequest> FillRequests { get; set; } = new List<FillRequest>();
 
-    public virtual Week? Week { get; set; }
+    public virtual Week? Week { get; set; } = null!;
 
     public string? EmployeeName => Employee?.Name ?? ExternalEmployeeName;
 }
