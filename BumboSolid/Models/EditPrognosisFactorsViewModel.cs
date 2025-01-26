992e17fb9b51f7fd2320e9bb04b0a0bba05c3b56
Janno <165158587+BaksteenV20@users.noreply.github.com>
2025-01-26 14:33:50 +0100

    prognosis validation

--- BumboSolid/Models/EditPrognosisFactorsViewModel.cs
+++ BumboSolid/Models/EditPrognosisFactorsViewModel.cs
@@ -6,17 +6,22 @@ namespace BumboSolid.Models;
 public class EditPrognosisFactorsViewModel
 {
     public required Week Prognosis { get; set; }
+
     public required List<Weather> WeatherValues { get; set; }
 
     // These properties hold the user input
     [Required(ErrorMessage = "Dit veld is vereist")]
     public int[] VisitorEstimates { get; set; } = null!;
+
     [Required(ErrorMessage = "Dit veld is vereist")]
     public int[] Holidays { get; set; } = null!;
+
     [Required(ErrorMessage = "Dit veld is vereist")]
     public int[] WeatherIds { get; set; } = null!;
+
     [Required(ErrorMessage = "Dit veld is vereist")]
     public int[] Others { get; set; } = null!;
+
     [Required(ErrorMessage = "Dit veld is vereist")]
     public string[] Descriptions { get; set; } = null!;
 }
